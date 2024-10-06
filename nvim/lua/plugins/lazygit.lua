ffaa64dac3ff8ed1e7fbcde3ba4c2235d4bae60e
recleun <98621222+recleun@users.noreply.github.com>
2024-10-06 15:35:25 +0300

    Change keybinding for lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -12,6 +12,6 @@ return {
         "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "<leader>lg", ":LazyGit<CR>", desc = "LazyGit" }
+        { "<C-g>", ":LazyGit<CR>", desc = "LazyGit" }
     }
 }
