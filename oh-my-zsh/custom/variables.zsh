690a26c35a9f7e7eca2cc4e423ffd7bc62562377
recleun <98621222+recleun@users.noreply.github.com>
2024-10-10 00:10:20 +0300

    Move environment variable to hyprland config

--- oh-my-zsh/custom/variables.zsh
+++ oh-my-zsh/custom/variables.zsh
@@ -3,7 +3,6 @@ export EDITOR=/usr/bin/nvim
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$HOME/scripts:$PATH
 export MANPATH="/usr/local/man:$MANPATH"
 export ARCHFLAGS="-arch $(uname -m)"
-export QT_QPA_PLATFORMTHEME="qt6ct"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
