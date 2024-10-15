36abb2f02a3c82777f6d765ad4ac88b8b595cc98
recleun <98621222+recleun@users.noreply.github.com>
2024-10-16 01:09:32 +0300

    Disable fish's greeting message

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -gx EDITOR /usr/bin/nvim
 set -gx PATH $HOME/bin $HOME/.local/bin /usr/local/bin $HOME/scripts $PATH
 set -gx ARCHFLAGS -arch (uname -m)
 set -gx NVM_DIR $HOME/.nvm
+set fish_greeting
 
 function nvm
     bash -c "source $NVM_DIR/nvm.sh; nvm $argv"
