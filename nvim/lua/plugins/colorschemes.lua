2d5140e6bc3a6b0328ecc315f4bca8cf5056a901
recleun <98621222+recleun@users.noreply.github.com>
2024-10-15 15:03:14 +0300

    Install tokyodark & onedark, change github theme

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -6,9 +6,28 @@ return {
             require("github-theme").setup({
                 options = {
                     transparent = true,
+                    terminal_colors = false,
                 },
             })
-            vim.cmd.colorscheme("github_dark_high_contrast")
+            vim.cmd.colorscheme("github_dark")
         end
+    },
+    {
+        'navarasu/onedark.nvim',
+        config = function()
+            require("onedark").setup({
+                options = {
+                    transparent = true,
+                },
+            })
+        end
+    },
+    {
+        "tiagovla/tokyodark.nvim",
+        config = function()
+            require("tokyodark").setup({
+                transparent_background = true,
+            })
+        end,
     }
 }
