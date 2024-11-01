6873971f4171d6414efe67c455a9104c672f1c01
recleun <98621222+recleun@users.noreply.github.com>
2024-11-01 12:23:20 +0200

    Remove nvm related configurations

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,8 @@
 set -gx EDITOR /usr/bin/nvim
 set -gx PATH $HOME/bin $HOME/.local/bin /usr/local/bin $HOME/scripts $PATH
 set -gx ARCHFLAGS -arch (uname -m)
-set -gx NVM_DIR $HOME/.nvm
 set fish_greeting
 
 fish_vi_key_bindings
-
-function nvm
-    bash -c "source $NVM_DIR/nvm.sh; nvm $argv"
-end
 
 starship init fish | source
