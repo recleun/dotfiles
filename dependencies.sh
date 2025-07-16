df01c621a619a41089324adf2b33734cd75b153a
recleun <98621222+recleun@users.noreply.github.com>
2025-07-16 04:58:09 +0300

    Add jq dependency

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -Sy neovim i3-wm xorg-xinit xorg-server xorg-xinput xorg-xsetroot lazygit kitty rofi vifm fastfetch clang deno starship discord unzip dunst flameshot xclip ripgrep fzf ttf-cascadia-mono-nerd playerctl openssh noto-fonts-cjk unclutter flatpak htop fish
+sudo pacman -Sy neovim i3-wm xorg-xinit xorg-server xorg-xinput xorg-xsetroot lazygit kitty rofi vifm fastfetch clang deno starship discord unzip dunst flameshot xclip ripgrep fzf ttf-cascadia-mono-nerd playerctl openssh noto-fonts-cjk unclutter flatpak htop fish jq
 
 curl https://sh.rustup.rs -sSf | sh
 
