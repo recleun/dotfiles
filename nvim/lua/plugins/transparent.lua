f8cc63efb19ade5be6635d7e8d57b7007a4997e7
recleun <98621222+recleun@users.noreply.github.com>
2025-07-28 06:58:18 +0300

    Add transparency to specific groups (sign groups)

--- nvim/lua/plugins/transparent.lua
+++ nvim/lua/plugins/transparent.lua
@@ -1,4 +1,18 @@
 return {
     'xiyaowong/transparent.nvim',
     lazy = false,
+    config = function()
+        require("transparent").setup({
+            extra_groups = {
+                "GitSignsAdd",
+                "GitSignsChange",
+                "GitSignsDelete",
+                "DiagnosticSignError",
+                "DiagnosticSignHint",
+                "DiagnosticSignInfo",
+                "DiagnosticSignOk",
+                "DiagnosticSignWarn",
+            }
+        })
+    end,
 }
