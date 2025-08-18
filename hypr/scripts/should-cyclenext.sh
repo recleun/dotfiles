e2f491b384cdd3bb48b79e8ceda1aa7f4e6b65f6
recleun <98621222+recleun@users.noreply.github.com>
2025-08-18 04:39:38 +0300

    Add the two modes of fullscreen

--- hypr/scripts/should-cyclenext.sh
+++ hypr/scripts/should-cyclenext.sh
@@ -2,9 +2,9 @@
 
 STATE=$(hyprctl activewindow -j | jq -r '.fullscreen')
 
-if [[ "$STATE" == "2" && "$1" == "right" ]]; then
+if [[ "$STATE" == "2" || "$STATE" == "1" && "$1" == "right" ]]; then
     hyprctl dispatch cyclenext
-elif [[ "$STATE" == "2" && "$1" == "left" ]]; then
+elif [[ "$STATE" == "2" || "$STATE" == "1" && "$1" == "left" ]]; then
     hyprctl dispatch cyclenext prev
 elif [[ "$1" == "right" ]]; then
     hyprctl dispatch movefocus r
