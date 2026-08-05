eef31c373600bcfa8ea58b1888d01458041424a7
recleun <98621222+recleun@users.noreply.github.com>
2026-08-06 02:53:47 +0300

    Add barbar.nvim

---
+++ nvim/lua/plugins/barbar.lua
@@ -0,0 +1,38 @@
+return {
+    "romgrk/barbar.nvim",
+    config = function()
+        vim.keymap.set("n", "<C-w>", vim.cmd.bdelete)
+
+        vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
+        vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
+        vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
+        vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
+        vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
+        vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
+        vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
+        vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
+        vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
+        vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>")
+
+        vim.g.barbar_auto_setup = false
+
+        require("barbar").setup({
+            animation = true,
+            auto_hide = 1,
+            clickable = false,
+            exclude_name = {
+                "fish",
+            },
+            focus_on_close = "right",
+            icons = {
+                buffer_index = true,
+                buffer_number = false,
+                current = { button = "" },
+                inactive = { button = "" },
+            },
+            insert_at_end = true,
+            maximum_padding = 1,
+            minimum_padding = 1,
+        })
+    end,
+}
