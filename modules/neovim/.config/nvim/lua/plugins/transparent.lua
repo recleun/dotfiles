a01a858abb1aa9624f251a212bf759076540e61a
recleun <98621222+recleun@users.noreply.github.com>
2026-08-17 06:55:14 +0300

    Add transparency to new things

--- modules/neovim/.config/nvim/lua/plugins/transparent.lua
+++ modules/neovim/.config/nvim/lua/plugins/transparent.lua
@@ -1,9 +1,9 @@
 return {
     'xiyaowong/transparent.nvim',
     lazy = false,
-    enabled = false,
     config = function()
-        require("transparent").setup({
+        local transparent = require("transparent")
+        transparent.setup({
             extra_groups = {
                 "GitSignsAdd",
                 "GitSignsChange",
@@ -13,7 +13,35 @@ return {
                 "DiagnosticSignInfo",
                 "DiagnosticSignOk",
                 "DiagnosticSignWarn",
+
+                "TabLineFill",
+
+                "StatusLine",
+                "StatusLineNC",
+                "lualine_a_normal",
+                "lualine_b_normal",
+                "lualine_c_normal",
+                "lualine_a_insert",
+                "lualine_b_insert",
+                "lualine_c_insert",
+                "lualine_a_visual",
+                "lualine_b_visual",
+                "lualine_c_visual",
+                "lualine_a_replace",
+                "lualine_b_replace",
+                "lualine_c_replace",
+                "lualine_a_command",
+                "lualine_b_command",
+                "lualine_c_command",
+                "lualine_a_inactive",
+                "lualine_b_inactive",
+                "lualine_c_inactive",
             }
         })
+
+        transparent.clear_prefix("Buffer")
+        transparent.clear_prefix("Navic")
+        transparent.clear_prefix("lualine")
+        transparent.clear_prefix("Telescope")
     end,
 }
