1ec0c03fcbfb8f3b26025bc0a74c9f058e984029
recleun <98621222+recleun@users.noreply.github.com>
2026-08-17 06:01:43 +0300

    Include files/folders that start with a dot

--- modules/neovim/.config/nvim/lua/plugins/telescope.lua
+++ modules/neovim/.config/nvim/lua/plugins/telescope.lua
@@ -19,6 +19,14 @@ return {
             local telescope = require("telescope")
             telescope.setup({
                 pickers = {
+                    find_files = {
+                        hidden = true,
+                    },
+                    live_grep = {
+                        additional_args = function(opts)
+                            return { "--hidden" }
+                        end,
+                    },
                     buffers = {
                         sort_lastused = true,
                         mappings = {
