5a2bd90d0bc07e8465aae13f2ad9672a3cd13473
recleun <98621222+recleun@users.noreply.github.com>
2026-08-24 04:53:27 +0300

    Add persistence.nvim plugin

---
+++ modules/neovim/.config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/persistence.nvim",
+    lazy = true,
+    event = "BufReadPre",
+    config = function()
+        local persistence = require("persistence")
+        persistence.setup({})
+        vim.keymap.set("n", "<leader>sl", function() persistence.load() end)
+        vim.keymap.set("n", "<leader>ss", function() persistence.select() end)
+    end,
+}
