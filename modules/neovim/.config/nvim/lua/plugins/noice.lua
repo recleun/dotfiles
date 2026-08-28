874a5c56f3f287d9eaeb4521ecdcb34f8494f438
recleun <98621222+recleun@users.noreply.github.com>
2026-08-28 06:42:24 +0300

    feat(notify): replace notifier with nvim-notify
    Plus set a background "colour"

--- modules/neovim/.config/nvim/lua/plugins/noice.lua
+++ modules/neovim/.config/nvim/lua/plugins/noice.lua
@@ -4,10 +4,15 @@ return {
     opts = {
         presets = {
             lsp_doc_border = true,
-        }
+        },
     },
     dependencies = {
         "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-    }
+        {
+            "rcarriga/nvim-notify",
+            opts = {
+                background_colour = "#000000"
+            },
+        },
+    },
 }
--- modules/neovim/.config/nvim/lua/plugins/snacks.lua
+++ modules/neovim/.config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,6 @@ return {
     priority = 1000,
     opts = {
         indent = { enabled = true },
-        notifier = { enabled = true },
         scroll = { enabled = true },
         dim = {
             scope = {
