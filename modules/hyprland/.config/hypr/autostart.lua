7fb598bbf9f1450c6a1f8d596835c27c5f470a44
recleun <98621222+recleun@users.noreply.github.com>
2026-09-09 21:08:01 +0300

    feat(hypr): change autostarted programs

--- modules/hyprland/.config/hypr/autostart.lua
+++ modules/hyprland/.config/hypr/autostart.lua
@@ -1,9 +1,8 @@
 hl.on("hyprland.start", function () 
     hl.exec_cmd("noctalia")
-    hl.exec_cmd("hyprsunset")
     hl.exec_cmd("discord")
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    hl.exec_cmd("flameshot")
-    hl.exec_cmd("zen-browser")
+    hl.exec_cmd("firefox")
+    hl.exec_cmd("spotify")
+    hl.exec_cmd("steam")
 end)
 
