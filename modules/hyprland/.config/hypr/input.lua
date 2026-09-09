8f004e91defcfda2377c62c8260b678ebc678930
recleun <98621222+recleun@users.noreply.github.com>
2026-09-09 21:07:14 +0300

    feat(hypr): disable mouse acceleration

--- modules/hyprland/.config/hypr/input.lua
+++ modules/hyprland/.config/hypr/input.lua
@@ -11,6 +11,7 @@ hl.config({
         follow_mouse = 1,
 
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+        accel_profile = "flat",
 
         touchpad = {
             natural_scroll = true,
